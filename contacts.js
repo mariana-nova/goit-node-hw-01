f80e62cee10ec8ec289934c50d7a7abf9207a3ab
mariana-nova <119463538+mariana-nova@users.noreply.github.com>
2023-10-11 21:57:25 -0500

    index arreglado

--- contacts.js
+++ contacts.js
@@ -1,29 +1,65 @@
-const fs = require('fs');
-const path = require('path');
-
-const contactsPath = path.join(__dirname, 'db', 'contacts.json');
-
+const fs = require("fs");
+const path = require("path");
+const contactsPath = path.join(__dirname, "db", "contacts.json"); 
 function listContacts() {
-    fs.readFile(contactsPath, 'utf8', (error, data) => {
-      if (error) {
-        console.error('Error reading contacts:', error);
-        return;
-      }
-      const contacts = JSON.parse(data);
-      console.table(contacts);
-    });
+
+  const contactsData = fs.readFileSync(contactsPath, "utf-8");
+  const contacts = JSON.parse(contactsData);
+
+  console.table(contacts);
+}
+
+function getContactById(id) {
+
+  const contactsData = fs.readFileSync(contactsPath, "utf-8");
+  const contacts = JSON.parse(contactsData);
+
+  const contact = contacts.find((c) => c.id === id);
+  if (contact) {
+    console.table([contact]);
+  } else {
+    console.log("Contact not found.");
   }
-  
-  function getContactById() {
+}
 
+function addContact(name, email, phone) {
+
+  const contactsData = fs.readFileSync(contactsPath, "utf-8");
+  const contacts = JSON.parse(contactsData);
+
+  const newContact = {
+    id: generateContactId(), // Implementa esta función para generar un ID único
+    name,
+    email,
+    phone,
+  };
+
+  contacts.push(newContact);
+
+  fs.writeFileSync(contactsPath, JSON.stringify(contacts, null, 2));
+
+  console.log("Contact added successfully.");
+}
+
+function removeContact(id) {
+
+  const contactsData = fs.readFileSync(contactsPath, "utf-8");
+  const contacts = JSON.parse(contactsData);
+
+  const index = contacts.findIndex((c) => c.id === id);
+  if (index !== -1) {
+    contacts.splice(index, 1);
+    fs.writeFileSync(contactsPath, JSON.stringify(contacts, null, 2));
+    console.log("Contact removed successfully.");
+  } else {
+    console.log("Contact not found.");
   }
+}
 
-  function removeContact () {
+function generateContactId() {
 
-  }
+}
 
-  function addContact (){
+module.exports = { listContacts, getContactById, addContact, removeContact };
 
-  }
- 
 
--- index.js
+++ index.js
@@ -1,69 +1,43 @@
-const yargs = require('yargs');
-const contacts = require('./contacts'); 
+const { Command } = require("commander");
+const program = new Command();
+const contacts = require("./contacts");
 
-const argv = yargs
-  .command('list', 'List all contacts')
-  .command('get', 'Get a contact by ID', {
-    id: {
-      describe: 'Contact ID',
-      demand: true,
-      alias: 'i'
-    }
-  })
-  .command('add', 'Add a new contact', {
-    name: {
-      describe: 'Contact name',
-      demand: true,
-      alias: 'n'
-    },
-    email: {
-      describe: 'Contact email',
-      demand: true,
-      alias: 'e'
-    },
-    phone: {
-      describe: 'Contact phone number',
-      demand: true,
-      alias: 'p'
-    }
-  })
-  .command('remove', 'Remove a contact by ID', {
-    id: {
-      describe: 'Contact ID',
-      demand: true,
-      alias: 'i'
-    }
-  })
-  .help()
-  .argv;
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
-  function invokeAction({ action, id, name, email, phone }) {
-    switch (action) {
-      case 'list':
-        
-        contacts.listContacts();
-        break;
-  
-      case 'get':
-       
-        contacts.getContactById(id);
-        break;
-  
-      case 'add':
-       
-        contacts.addContact(name, email, phone);
-        break;
-  
-      case 'remove':
-   
-        contacts.removeContact(id);
-        break;
-  
-      default:
-        console.warn('\x1B[31m Unknown action type!');
-    }
+program.parse(process.argv);
+
+const argv = program.opts();
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+     
+      contacts.listContacts();
+      break;
+
+    case "get":
+
+      contacts.getContactById(id);
+      break;
+
+    case "add":
+
+      contacts.addContact(name, email, phone);
+      break;
+
+    case "remove":
+    
+      contacts.removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
   }
-  
+}
 
-  invokeAction(argv);
-  
+invokeAction(argv);
